da25b2d78357136c7599386a3d861150da845e97
Egor <ausioff@gmail.com>
2021-03-23 15:11:06 +0300

    deleted all

--- react-app/src/redux/sideBar-reducer.js
+++
@@ -1,6 +0,0 @@
-let initialState = {friendsNames: ['Andrew', 'Sasha', 'Sveta', 'Gosha', 'Corin']};
-const sideBarReducer = (state = initialState, action) => {
-
-    return state;
-}
-export  default  sideBarReducer;
